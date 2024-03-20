11c40508311553b008df7521b5b75f5f5f8acb66
Gaozhi Wang <37297312@qq.com>
2024-03-20 23:41:55 +0800

    增加json解析属性，使json对象和实体对象对应

--- src/main/java/com/akshare/data/entity/StockInfoSh.java
+++ src/main/java/com/akshare/data/entity/StockInfoSh.java
@@ -1,5 +1,7 @@
 package com.akshare.data.entity;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -12,21 +14,33 @@ import org.hibernate.annotations.Comment;
 @Comment("上海证券交易所股票代码和简称数据")
 @Table(name = "stock_info_sh")
 public class StockInfoSh {
+
     @Id
     @Column(name = "code", unique = true)
     @Comment("股票代码")
+    @JsonAlias({"证券代码"})
     public String code;
 
     @Column(name = "name")
     @Comment("证券简称")
+    @JsonAlias({"证券简称"})
     public String name;
 
-    @Column(name = "full_name")
     @Comment("证券全称")
+    @Column(name = "full_name")
+    @JsonAlias({"公司全称"})
     public String fullName;
 
     @Column(name = "launch_date")
     @Comment("上市日期")
+    @JsonAlias({"上市日期"})
     public String launchDate;
+
+    @Column(name = "block_type")
+    @Comment("板块类型")
+    @JsonAlias({"板块类型"})
+    public String blockType;
+
+
 
 }
